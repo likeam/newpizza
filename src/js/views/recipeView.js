7b88eef20a3436bfca33be695b5a2439ee8b5fbc
AbdulRehman <likeam99@gmail.com>
2022-02-22 10:52:18 +0500

    ok

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,22 +1,37 @@
+import icons from 'url:../../img/icons.svg'; 
+
  class RecipeView{
     #parentElement = document.querySelector('.recipe');
 
     render(data){
-        this.#data = data;
-        const markup = this.#generateMarkup;
-        this.#clear;
+        this._data = data;
+        const markup = this.#generateMarkup();
+        this.#clear();
         this.#parentElement.insertAdjacentHTML('afterbegin',  markup);
     }
     #clear(){
         this.#parentElement.innerHTML = '';
     }
 
+    renderSpinner = function() {
+      const markup = `
+            <div class="spinner">
+              <svg>
+                <use href="${icons}#icon-loader"></use>
+              </svg>
+            </div> 
+      `;
+      this.#parentElement.innerHTML = '';
+      this.#parentElement.insertAdjacentHTML('afterbegin', markup);
+    };
+
     #generateMarkup(){
+      console.log(this._data);
         return ` 
         <figure class="recipe__fig">
-        <img src="${this.#data.image}" alt="Tomato" class="recipe__img" />
+        <img src="${this._data.image}" alt="Tomato" class="recipe__img" />
         <h1 class="recipe__title">
-          <span>${this.#data.title}</span>
+          <span>${this._data.title}</span>
         </h1>
         </figure>
           <div class="recipe__details">
@@ -24,14 +39,14 @@
         <svg class="recipe__info-icon">
           <use href="${icons}#icon-clock"></use>
         </svg>
-        <span class="recipe__info-data recipe__info-data--minutes">${this.#data.cookingTime}</span>
+        <span class="recipe__info-data recipe__info-data--minutes">${this._data.cookingTime}</span>
         <span class="recipe__info-text">minutes</span>
       </div>
       <div class="recipe__info">
         <svg class="recipe__info-icon">
           <use href="${icons}#icon-users"></use>
         </svg>
-        <span class="recipe__info-data recipe__info-data--people">${this.#data.servings}</span>
+        <span class="recipe__info-data recipe__info-data--people">${this._data.servings}</span>
         <span class="recipe__info-text">servings</span>
         <div class="recipe__info-buttons">
           <button class="btn--tiny btn--increase-servings">
@@ -60,7 +75,7 @@
     <div class="recipe__ingredients">
       <h2 class="heading--2">Recipe ingredients</h2>
       <ul class="recipe__ingredient-list">
-        ${this.#data.ingredients.map(ing => {
+        ${this._data.ingredients.map(ing => {
           return `
          
           <li class="recipe__ingredient">
@@ -82,7 +97,7 @@
         <h1 class="heading--2">How to cook it</h1>
         <p class="recipe__directions-text">
           This recipe was carefully designed and tested by
-          <span class="recipe__publisher">${this.#data.publisher}</span>. Please check out
+          <span class="recipe__publisher">${this._data.publisher}</span>. Please check out
           directions at their website.
         </p>
         <a
