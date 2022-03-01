dee7a714120181a4f576a497598550f5fae4e23d
AbdulRehman <likeam99@gmail.com>
2022-03-01 23:38:02 +0500

    ok

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,18 +1,18 @@
 class SearchView{
-    _parentEl = document.querySelector('.search');
+    _parentElementement = document.querySelector('.search');
     _errorMassage = 'WE could not find that recipe. Please try anothe one';
     _massage = '';
 
     getQuery(){
-        const query = this._parentEl.querySelector('.search__field').value;
+        const query = this._parentElement.querySelector('.search__field').value;
         this._clearInput();
         return query; 
     }
     _clearInput(){
-        this._parentEl.querySelector('.search__field').value = '';
+        this._parentElement.querySelector('.search__field').value = '';
     }
     addHandlerSearch(handler){
-        this._parentEl.addEventListener('submit', function(e){
+        this._parentElement.addEventListener('submit', function(e){
             e.preventDefault();
             handler(); 
         });
